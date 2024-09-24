8dbef7ef765cbe9d1b89dda6d6530eb56fc60ec4
Lasha Maruashvili <maruashvililasha@gmail.com>
2024-09-24 11:34:32 +0400

    Initial Commit

---
+++ RickAndMorthy/RickAndMorthyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMorthyApp.swift
+//  RickAndMorthy
+//
+//  Created by Lasha Maruashvili on 24.09.24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMorthyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
